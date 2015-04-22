e92d7ba87c1723481ac94f83e4f4a2e0f84767e0
Aaron Harder <aaron@entropy.cc>
2015-04-22 13:41:07 -0500

    Support for listening on multiple ports simultaneously, one per run-mode.  Use different sling source depending on port of originating request.

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+var http=require("http");	
 
 module.exports.express = require('./lib/blacklight-express');
 module.exports.componentTypes = require('./lib/component-types');
-module.exports._ = require("lodash");
+var _ = module.exports._ = require("lodash");
+
+module.exports.connectionsByRunMode = function(configDictionary){
+
+	return (function(){
+		var scs={modes: {}, ports:{}};
+
+		_.each(configDictionary,function(val, idx){
+			var sc=new SlingConnector(val);
+			sc.runMode = idx;
+			sc.blacklightPort = val.port;
+			scs.modes[idx]=sc;
+			scs.ports[val.port]=sc;
+		});
+
+		return scs;
+	})();
+}
+
+
+
+module.exports.launchHttp = function(app, slingConnectors){
+	_.each(slingConnectors.ports, function(val,port){
+		console.log("Launching [" + val.runMode + "] listener on port [" + port + "]");
+		http.createServer(app).listen(port);
+	});
+}
